a5e4136163c311d90b78288166339bc7a1073268
Oriol López-Massaguer <oriol.lopez@gmail.com>
2018-01-29 12:17:05 +0100

    full inference

--- database/full_inference.sql
+++ database/full_inference.sql
@@ -1,4 +1,7 @@
 -- Full ontologies inference
+create table infer_findings_all_complete as
+select findings_all where organ_normalised is not null and observation_normalised is not null;
+
 drop table infer_all_organs;
 create table infer_all_organs as select child_term,parent_term,super_parent from label_params('anatomical entity');
 
@@ -20,6 +23,10 @@ select distinct
 	infer_all_morph_changes.parent_term inferred_observation 
 from infer_hpf_organs left join infer_all_morph_changes on infer_hpf_organs.observation_normalised=infer_all_morph_changes.child_term;
 
+select count(*) from infer_hpf_organs_morph_changes where inferred_organ =organ_normalised;
+select count(*) from infer_hpf_organs_morph_changes where inferred_observation =observation_normalised;
+
+select * from infer_hpf_organs_morph_changes where inferred_observation is null limit 10;
 
 
 select count(*) from infer_hpf_organs;
