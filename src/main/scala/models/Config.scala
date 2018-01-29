c3db82c766c95915d444ee3b4068e684f9fb0e3d
Oriol López Massaguer <oriol.lopez@gmail.com>
2018-01-29 14:50:17 +0100

    eliminare home path

--- src/main/scala/models/Config.scala
+++ src/main/scala/models/Config.scala
@@ -6,7 +6,6 @@ object Config {
 
   val cf = ConfigFactory.parseFile(new java.io.File("conf/application.conf"))
   val home_path = cf.getString("home.path")
-  println("Home path: " + home_path)
   val dbURLetox = cf.getString("etoxdb.url")
   val dbUseretox = cf.getString("etoxdb.user")
   val dbPasswordetox = cf.getString("etoxdb.password")
