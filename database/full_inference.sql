1435b3ef503f715496777b3665c38cc6ad8f1201
Oriol López-Massaguer <oriol.lopez@gmail.com>
2018-01-29 12:01:53 +0100

    full inference

--- database/full_inference.sql
+++ database/full_inference.sql
@@ -4,7 +4,7 @@ create table infer_all_organs as select child_term,parent_term,super_parent from
 create table infer_all_morph_changes as select child_term,parent_term,super_parent from label_params('morphologic change'); 
 
 create table infer_hpf_organs as
-	select 
+	select distinct
 		findings_all.*, 
 		infer_all_organs.parent_term inferred_organ
 	from findings_all left join infer_all_organs on findings_all.organ_normalised=infer_all_organs.child_term
@@ -12,13 +12,13 @@ create table infer_hpf_organs as
 
 
 create table infer_hpf_organs_morph_changes as
-select 
+select distinct
 	infer_hpf_organs.*,
 	infer_all_morph_changes.parent_term inferred_observation 
 from infer_hpf_organs left join infer_all_morph_changes on infer_hpf_organs.observation_normalised=infer_all_morph_changes.child_term;
 
 
 
-select count(*) from infer_hpf_organs
+select count(*) from infer_hpf_organs;
 
-select count(*) from infer_hpf_organs_morph_changes
+select count(*) from infer_hpf_organs_morph_changes;
